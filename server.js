67587b9a7096099aefb0faedbde3616e9581f30d
AndradeTK <devandradetk@gmail.com>
2024-10-04 18:47:20 -0300

    add

--- server.js
+++ server.js
@@ -297,8 +297,8 @@ app.use((err, req, res, next) => {
   res.status(500).render('error404', { erro: 500 });
   
 });
-
-app.listen(3000, function(erro) {
+var port = process.env.PORT || 3000
+app.listen(port, function(erro) {
   if(erro){
       console.log("❌ » Erro :" + erro)
   }
